650e7d2143ed4cad26eee4597a01296b554b9650
Anjan <anjanhr1997@gmail.com>
2022-12-26 13:11:40 +0530

    removed s3

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,44 +3,44 @@ const User = require("../models/user");
 const Budget = require("../models/budget");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+// require("dotenv").config();
 
-const { S3Client } = require("@aws-sdk/client-s3");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
+// const { S3Client } = require("@aws-sdk/client-s3");
+// const multer = require("multer");
+// const multerS3 = require("multer-s3");
 
-const S3_ACCESS_KEY_ID = "AKIAUVKMMK3W24WBYYKN";
-const S3_SECRET_ACCESS_KEY = "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";
-const S3_BUCKET_REGION = "ap-south-1";
-const S3_BUCKET_NAME = "new-expense-profile";
+// const S3_ACCESS_KEY_ID = "AKIAUVKMMK3W24WBYYKN";
+// const S3_SECRET_ACCESS_KEY = "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";
+// const S3_BUCKET_REGION = "ap-south-1";
+// const S3_BUCKET_NAME = "new-expense-profile";
 
 const userController = {};
 
-const s3 = new S3Client({
-  region: S3_BUCKET_REGION,
-  credentials: {
-    accessKeyId: S3_ACCESS_KEY_ID,
-    secretAccessKey: S3_SECRET_ACCESS_KEY,
-    region: S3_BUCKET_REGION,
-  },
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-  signatureVersion: "v4",
-});
+// const s3 = new S3Client({
+//   region: S3_BUCKET_REGION,
+//   credentials: {
+//     accessKeyId: S3_ACCESS_KEY_ID,
+//     secretAccessKey: S3_SECRET_ACCESS_KEY,
+//     region: S3_BUCKET_REGION,
+//   },
+//   sslEnabled: false,
+//   s3ForcePathStyle: true,
+//   signatureVersion: "v4",
+// });
 
-const upload = (bucketName) =>
-  multer({
-    storage: multerS3({
-      s3,
-      bucket: bucketName,
-      metadata: function (req, file, cb) {
-        cb(null, { fieldName: file.fieldname });
-      },
-      key: function (req, file, cb) {
-        cb(null, `image-${Date.now()}.jpeg`);
-      },
-    }),
-  });
+// const upload = (bucketName) =>
+//   multer({
+//     storage: multerS3({
+//       s3,
+//       bucket: bucketName,
+//       metadata: function (req, file, cb) {
+//         cb(null, { fieldName: file.fieldname });
+//       },
+//       key: function (req, file, cb) {
+//         cb(null, `image-${Date.now()}.jpeg`);
+//       },
+//     }),
+//   });
 
 userController.register = (request, response) => {
   const body = pick(request.body, ["email", "password"]);
@@ -102,51 +102,51 @@ userController.account = (request, response) => {
   response.json(request.tokenData);
 };
 
-userController.create = (request, response) => {
-  // this is the main req, res
-  const uploadSingle = upload(S3_BUCKET_NAME).single("image");
+// userController.create = (request, response) => {
+//   // this is the main req, res
+//   const uploadSingle = upload(S3_BUCKET_NAME).single("image");
 
-  uploadSingle(request, response, (error) => {
-    // this req, res are refering the same of above req, res
-    if (error) {
-      response.json({ amazonS3Error: error.message });
-    } else {
-      const name = request.body.name;
-      const occupation = request.body.occupation;
-      const profileUrl = request.file.location && request.file.location;
+//   uploadSingle(request, response, (error) => {
+//     // this req, res are refering the same of above req, res
+//     if (error) {
+//       response.json({ amazonS3Error: error.message });
+//     } else {
+//       const name = request.body.name;
+//       const occupation = request.body.occupation;
+//       const profileUrl = request.file.location && request.file.location;
 
-      User.findOneAndUpdate(
-        { _id: request.tokenData._id },
-        {
-          $set: {
-            "profile.name": name,
-            "profile.occupation": occupation,
-            "profile.image": profileUrl,
-          },
-        },
-        { new: true, runValidators: true }
-      )
-        .then((users) => {
-          response.json(users);
-        })
-        .catch((error) => {
-          if (error.message.includes("shorter")) {
-            response.json({
-              lengthError: "Check the min length (name & occupation [min:5])",
-            });
-          } else if (error.message.includes("longer")) {
-            response.json({
-              lengthError: "Check the max length (name & occupation [max:20])",
-            });
-          } else {
-            response.json({
-              mainError: error.message,
-            });
-          }
-        });
-    }
-  });
-};
+//       User.findOneAndUpdate(
+//         { _id: request.tokenData._id },
+//         {
+//           $set: {
+//             "profile.name": name,
+//             "profile.occupation": occupation,
+//             "profile.image": profileUrl,
+//           },
+//         },
+//         { new: true, runValidators: true }
+//       )
+//         .then((users) => {
+//           response.json(users);
+//         })
+//         .catch((error) => {
+//           if (error.message.includes("shorter")) {
+//             response.json({
+//               lengthError: "Check the min length (name & occupation [min:5])",
+//             });
+//           } else if (error.message.includes("longer")) {
+//             response.json({
+//               lengthError: "Check the max length (name & occupation [max:20])",
+//             });
+//           } else {
+//             response.json({
+//               mainError: error.message,
+//             });
+//           }
+//         });
+//     }
+//   });
+// };
 
 userController.destroy = (request, response) => {
   User.findOneAndUpdate(
