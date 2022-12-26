c28dc30dd8f8178b2e33e0f15547b4c10099105d
Anjan <anjanhr1997@gmail.com>
2022-12-26 05:37:12 +0530

    changed parser

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ configureDb();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(port, () => {
